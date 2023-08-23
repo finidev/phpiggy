9b64278c9f232f1a6743d0356c9a7938f9f40a12
Daniel Canup <daniel@finidev.tech>
2023-08-23 13:54:36 -0600

    The next installment
    This includes all the updated routes, containers, and more.

---
+++ src/App/Config/Middleware.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Config;
+
+use Framework\App;
+
+function registerMiddleware(App $app) {}
--- src/App/bootstrap.php
+++ src/App/bootstrap.php
@@ -5,7 +5,13 @@ declare(strict_types=1);
 require __DIR__ . "/../../vendor/autoload.php";
 
 use Framework\App;
+use App\Config\Paths;
 
-$app = new App();
+use function App\Config\{registerRouts, registerMiddleware};
+
+$app = new App(Paths::SOURCE . "App/container-definitions.php");
+
+registerRouts($app);
+registerMiddleware($app);
 
 return $app;
